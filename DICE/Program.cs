82976d38c119fe2e0de06bdeb58cc39603d59aa4
Alfons Stoltz <alfons.stoltz@learnet.se>
2021-10-01 12:47:50 +0200

    Add project files.

---
+++ DICE/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DICE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(); 
+            Console.WriteLine("How many dice will thou cast?"); 
+            try 
+            {
+                int times = Convert.ToInt32(Console.ReadLine()); 
+                Console.WriteLine("How many sides on them dice?"); 
+                int highest = Convert.ToInt32(Console.ReadLine()); 
+                Console.WriteLine("Rolling..."); 
+                for (int i = times; i > 0; i--) 
+                {
+                    Console.WriteLine(random.Next(1, highest + 1)); 
+                }
+            }
+            catch(Exception) 
+            {
+                Console.WriteLine("ERROR 404"); 
+            }
+        }
+    }
+}
